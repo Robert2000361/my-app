c4a18aacc56500836cc1dafcf0c27dfb68d1fac5
robert <robertkamal152@yahoo.com>
2026-05-08 15:52:41 +0300

    fix: update test to match new message

--- app.test.js
+++ app.test.js
@@ -1,11 +1,11 @@
 const http = require('http');
 const server = require('./app');
-test('returns Hello from CI/CD!', (done) => {
+test('returns CI/CD is Awesome and Automatic!!', (done) => {
   http.get('http://localhost:3000', (res) => {
     let data = '';
     res.on('data', chunk => data += chunk);
     res.on('end', () => {
-      expect(data).toBe('Hello from CI/CD!\n');
+      expect(data).toBe('CI/CD is Awesome and Automatic!!\n');
       server.close();
       done();
     });
